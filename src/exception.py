168344f58245d6bb1be0a78b80446e6face1c930
pkumv1 <pkumv1@gmail.com>
2024-03-09 17:32:07 +0530

    logging & exception

---
+++ src/exception.py
@@ -0,0 +1,25 @@
+import sys
+import logging
+
+def error_message_details(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name= exc_tb.frame.f_code.co_filename
+    error_message='this error occoured because of python script filename[{0}], line no[{1}] message [{2}]'.format(
+        file_name,exc_tb.lineno,str(error)
+    )
+return error_message
+    
+class CustomEXception(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(self,error_message)
+        self.error_message=error_message_details(error_message,error_detail=error_detail)
+    def __str_-(self):
+        return self.error_message
+
+#if __name__ =="__main__":
+  
+#    try:
+#        a=1/0
+#    except:
+#        logging.info("divide by zero error")
+#        raise CustomEXception
